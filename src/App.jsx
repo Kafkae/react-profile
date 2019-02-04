e9d35e3cba828b90cb63b8e4816a7448c80784fd
lesha.pros.1998@bk.ru <dark.revo4@mail.ru>
2019-02-04 22:18:38 +0300

    инит, eslint, глобальные стили, webpack

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import GlobalStyle from './components/styles/GlobalStyle';
+
+const App = () => (
+  <Fragment>
+    <div>hello</div>
+    <GlobalStyle />
+  </Fragment>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+/* eslint-disable */
+/* webpack.config.js */
+
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  devtool: false,
+  entry: {
+    app: [
+      './src/index.js',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[contenthash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin([{
+      from: './src/assets',
+      to: './assets',
+    }]),
+  ],
+
+  resolve: {
+    extensions: ['.js', '.jsx', 'webpack.js', '.web.js', '.mjs', '.json'],
+  },
+
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto",
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            name: './assets/fonts/[name].[ext]',
+          },
+        }],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '../assets/images/[name].[ext]',
+          },
+        }],
+      },
+    ],
+  },
+};
